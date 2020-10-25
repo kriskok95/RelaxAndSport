a9fcda77e76f711afb97f6cfca02397e50c48ab2
kriskok95 <kriskobota@abv.bg>
2020-10-25 22:32:16 +0200

    Added MassagesSchedule and TrainingsSchedule seeders.

---
+++ src/Server/RelaxAndSport.Domain/Booking/Models/MassagesSchedule/MassagesSchedule.Data.cs
@@ -0,0 +1,17 @@
+﻿namespace RelaxAndSport.Domain.Booking.Models.MassagesSchedule
+{
+    using RelaxAndSport.Domain.Common;
+    using System;
+    using System.Collections.Generic;
+
+    public class MassagesScheduleData : IInitialData
+    {
+        public Type EntityType => typeof(MassagesSchedule);
+
+        public IEnumerable<object> GetData()
+            => new List<MassagesSchedule>
+            {
+                new MassagesSchedule()
+            };
+    }
+}
---
+++ src/Server/RelaxAndSport.Domain/Booking/Models/TrainingsSchedule/TrainingsSchedule.Data.cs
@@ -0,0 +1,17 @@
+﻿namespace RelaxAndSport.Domain.Booking.Models.TrainingsSchedule
+{
+    using RelaxAndSport.Domain.Common;
+    using System;
+    using System.Collections.Generic;
+
+    public class TrainingsScheduleData : IInitialData
+    {
+        public Type EntityType => typeof(TrainingsSchedule);
+
+        public IEnumerable<object> GetData()
+            => new List<TrainingsSchedule>
+            {
+                new TrainingsSchedule()
+            };
+    }
+}
